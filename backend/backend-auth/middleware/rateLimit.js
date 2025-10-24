ede39101fb7e66e110dfc6cc25e8872e2ba39f60
Pham Cao Kiet <kiet@domain.com>
2025-10-24 13:28:28 +0700

    Thêm Redux và Protected Routes ok

--- backend/backend-auth/middleware/rateLimit.js
+++ backend/backend-auth/middleware/rateLimit.js
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit";
 
 export const loginRateLimiter = rateLimit({
-  windowMs: 5 * 60 * 1000, 
+  windowMs: 10 * 60 * 1000, 
   max: 5, // chỉ cho phép 5 lần login sai
   message: {
     success: false,
